60adeaed3f5d742100e1a6b00c90d53981b69b03
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2026-02-08 05:03:27 +0100

    created the project

---
+++ lib/pages/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          backgroundColor: Color(0xFFe4e0f4),
+          body: Container(
+        child: Column(
+          children: [
+          ],
+        )
+      )
+    );
+  }
+}
