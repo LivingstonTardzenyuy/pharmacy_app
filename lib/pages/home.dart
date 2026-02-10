7b79722649dbd81f7c573335b37598cc6faa56be
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2026-02-10 13:42:11 +0100

    workin gon UI

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool allmedicine = true, suppliment = false, vitamins=false, herbal=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,33 +29,194 @@ class _HomeState extends State<Home> {
                 Text( "Online Pharmacy",
                   style: AppWidget.lightTextStyle(28)
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(right: 25,),
-                        padding: EdgeInsets.only(right: 48, left: 20),
-
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                            borderRadius: BorderRadius.circular(30),
-                            border: Border.all(color: Colors.white, width: 1.5)
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Container(
+                              // margin: EdgeInsets.only(right: 30,),
+                              padding: EdgeInsets.only(right: 48, left: 20),
+                              height: 70,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                  borderRadius: BorderRadius.circular(30),
+                                  border: Border.all(color: Colors.white, width: 1.5)
+                              ),
+                              child: Center(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Search Medicine....",
+                                    hintStyle: AppWidget.lightTextStyle(16),
+                                    suffixIcon: Container(
+                                      padding: const EdgeInsets.only(right: 1),
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          borderRadius: BorderRadius.circular(18)
+                                      ),
+                                      child: Icon(Icons.search, color: Colors.white),
+                                    )
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Search Medicine",
-                            hintStyle: AppWidget.lightTextStyle(16)
+                      ),
+
+
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 20,),
+                Container(
+                  height: 60,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      // AppWidget.selectedCategory("All Medicine"),
+                      allmedicine ? AppWidget.selectedCategory("All Medicines") : GestureDetector(
+                        onTap: () {
+                          allmedicine = true;
+                          suppliment = false;
+                          vitamins = false;
+                          herbal = false;
+                          
+                          setState(() {
+
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                                "All Medicine",
+                              style: AppWidget.lightTextStyle(20.0)
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(color: Colors.black),
-                      child: Icon(Icons.search, color: Colors.white),
-                    )
-                  ],
+                      const SizedBox(width: 10,),
+                      suppliment ? AppWidget.selectedCategory("Suppliment") : GestureDetector(
+                        onTap: () {
+                          allmedicine = false;
+                          suppliment = true;
+                          vitamins = false;
+                          herbal = false;
+
+                          setState(() {
+
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                                "Suppliment",
+                              style: AppWidget.lightTextStyle(20.0)
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10,),
+                      vitamins ? AppWidget.selectedCategory("Vitamins") : GestureDetector(
+                        onTap: () {
+                          allmedicine = false;
+                          suppliment = false;
+                          vitamins = true;
+                          herbal = false;
+
+                          setState(() {
+
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                                "Vitamins",
+                                style: AppWidget.lightTextStyle(20.0)
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(width: 10,),
+                      herbal ? AppWidget.selectedCategory("Herbal") : GestureDetector(
+                        onTap: () {
+                          allmedicine = false;
+                          suppliment = false;
+                          vitamins = false;
+                          herbal = true;
+
+                          setState(() {
+
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                                "Herbal",
+                                style: AppWidget.lightTextStyle(20.0)
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 15,),
+                Container(
+                    margin: const EdgeInsets.only(right: 30.0),
+                    width: MediaQuery.of(context).size.width,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      // color: Color(0xffbab3a6)
+                      gradient: LinearGradient(colors: [Color(0xffbab3a6), Color(0xffddd7cb), Color(0xffa59c8f)], begin: Alignment.topLeft, end: Alignment.bottomRight)
+                    // color: Color(0xFFbab3a6)
+                  ),
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Center(
+                            child: Image.asset(
+                                "assets/images/steak.jpeg",
+                                height: 200, fit: BoxFit.cover,
+                            )
+                        ),
+                      ),
+                      const SizedBox(height: 10,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(color: Color(0xffc8c1a5)),
+                            width: MediaQuery.of(context).size.width,
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                    "Unique Medicine",
+                                    style: AppWidget.whiteTextStyle(20)
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
                 )
+
               ],
         )
       )
--- lib/widgets/support_widget.dart
+++ lib/widgets/support_widget.dart
@@ -11,4 +11,33 @@ class AppWidget{
       color: Colors.black, fontSize: size, fontFamily: "Poppins1"
     );
   }
+
+  static Widget selectedCategory(String name){
+    return Material(
+      elevation: 3.0,
+      borderRadius: BorderRadius.circular(30),
+      child: Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(20)
+          ),
+          child: Center(
+            child: Text(name, style: TextStyle(
+                fontFamily: "Poppins ",
+                fontSize: 18,
+                color: Colors.white
+            )),
+          )
+      ),
+    );
+  }
+
+
+
+  static TextStyle whiteTextStyle(double size){
+    return TextStyle(
+        color: Colors.white, fontSize: size, fontFamily: "Poppins"
+    );
+  }
 }
